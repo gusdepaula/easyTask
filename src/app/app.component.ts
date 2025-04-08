8ace3648459bf487093bdb7a6d386e89aeda54ef
Gustavo de Paula <gdepaula@gmail.com>
2025-04-08 10:12:09 -0300

    add header component with styles and template for improved UI

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
-import { HeaderComponent } from './header/header.component';
+import { HeaderComponent } from './components/header/header.component';
 import { UsersComponent } from './users/users.component';
 
 @Component({
